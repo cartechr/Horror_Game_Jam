30a05ea6308fcaa8310fe737197ee510c9093f4c
TheBossa <74020301+TheBossaaa@users.noreply.github.com>
2024-01-16 21:11:50 +0300

    v0.0.3 Worked on AI

--- Mannequin Horror Jam/Assets/Game Assets/Characters/Mannequins/Scripts/EnemyController.cs
+++ Mannequin Horror Jam/Assets/Game Assets/Characters/Mannequins/Scripts/EnemyController.cs
@@ -2,89 +2,196 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Animations.Rigging;
 
 public class EnemyController : MonoBehaviour
 {
 
-    [SerializeField] float movementSpeed;
+    [Header("Patrol Point Assignments")]
+    [Tooltip("Assign Relevant Patrolling Waypoints")]
+    [SerializeField] Transform[] patrolWaypoints;
+
+    [Header("Enemy Control Assignments")]
+    [Tooltip("Assign Enemy Object")]
+    [SerializeField] GameObject mannequinEnemy;
+    [Tooltip("Assign Player Here")]
+    [SerializeField] GameObject player;
+    [Tooltip("Eyes to emit raycast from")]
+    [SerializeField] GameObject eyes;
+    [Tooltip("Assign the NavMesh Agent (It's inside the mannequin)")]
     [SerializeField] NavMeshAgent navMeshAgent;
-    [SerializeField] Transform player;
-    [SerializeField] LayerMask whatIsGround, whatIsPlayer, whatIsWall;
+    [Tooltip("Assign the Enemy Animator Component")]
+    [SerializeField] Animator enemyAnimatorController;
+    [Tooltip("This will be pulled automatically")]
+    [SerializeField] MultiAimConstraint multiAimConstraint;
+    [Tooltip("Distance for eyes to see")]
+    [SerializeField] float raycastDistance;
+    [Tooltip("Assign the speed for the enemy")]
+    [SerializeField] float moveSpeed;
+    [Tooltip("Delay between waypoints")]
+    [SerializeField] float delayBetween;
+    [Tooltip("Float variable for head turn DO NOT CHANGE")]
+    [SerializeField] float weight;
+    [Tooltip("Head turn weight increase duration")]
+    [SerializeField] float increaseDuration = 3f;
+    [Tooltip("This is for debugging and animation triggering")]
+    [SerializeField] bool isMoving;
+    [Tooltip("This is for debugging and animation triggering")]
+    [SerializeField] bool isChasingPlayer;
 
-    //Patrolling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
 
-
-    //States
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
-
-    private void Awake()
+    //Once the object enabled, do this
+    private void OnEnable()
     {
-        //playerInAttackRange = GameObject.FindWithTag("Player").transform;
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        ReturnToStart();
+        
+        
     }
 
-
+    private void Start()
+    {
+        HeadIKSetup();
+        StartCoroutine(FollowPath());
+    }
 
     private void Update()
     {
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-
-        if (!playerInSightRange && !playerInAttackRange) Patroling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-
-    }
-
-    private void Patroling()
-    {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-            navMeshAgent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f )
-        {
-            walkPointSet = false;
-            SearchWalkPoint();
-        }
-        
-        if(Physics.Raycast(transform.position, walkPoint - transform.position, out RaycastHit hit, 2f, whatIsWall))
-        {
-            walkPointSet = false;
-            SearchWalkPoint();
-        }
+        PerformRaycast();
+        ChasePlayer();
         
     }
-    private void SearchWalkPoint()
+
+    void ReturnToStart()
     {
-        //Calculate random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-            walkPointSet = true;
+        enemyAnimatorController.SetBool("isMoving", false);
+        if (patrolWaypoints.Length > 0)
+            transform.position = patrolWaypoints[0].position;
     }
 
-    private void ChasePlayer()
+    IEnumerator FollowPath()
     {
-        navMeshAgent.SetDestination(player.position);
+
+        while (!isChasingPlayer) //Added a considiton to exit the loop
+        {
+            foreach (Transform waypoint in patrolWaypoints)
+            {
+                Vector3 startPosition = transform.position;
+                Vector3 endPosition = waypoint.transform.position;
+                float travelPercent = 0f;
+
+                transform.LookAt(endPosition);
+
+                while (travelPercent < 1f)
+                {
+
+                    isMoving = true;
+                    Debug.Log("Moving State: " + isMoving);
+                    enemyAnimatorController.SetBool("isMoving", true);
+                    travelPercent += Time.deltaTime * moveSpeed;
+                    transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                    yield return new WaitForEndOfFrame();
+
+                }
+                isMoving = false;
+                Debug.Log("MovingState: " + isMoving);
+                enemyAnimatorController.SetBool("isMoving", false);
+
+                if (!isChasingPlayer) // Check again before the delay to ensure we don't continue if isChasingPlayer became true during the move
+                    yield return new WaitForSeconds(delayBetween); // Optional delay between waypoints
+
+                if (isChasingPlayer) StopCoroutine(FollowPath());
+            }
+        }
+
     }
 
-
-    private void OnDrawGizmosSelected()
+    void PerformRaycast()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Ray ray = new Ray(eyes.transform.position, eyes.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, raycastDistance))
+        {
+            // If the ray hits something, you can check what it hit
+            Debug.Log("Ray hit: " + hit.collider.gameObject.name);
+
+            // Here, you can implement logic to handle what to do when the ray hits something
+            // For example, check if it hit the player and initiate chase behavior
+            if (hit.collider.CompareTag("Player"))
+            {
+                isChasingPlayer = true;
+                HeadLockOnTarget();
+                Debug.Log("Player Detected! Start Chasing...");
+                // Implement logic to start chasing the player
+            }
+            else
+            {
+                isChasingPlayer = false;
+            }
+        }
+    }
+
+    void ChasePlayer()
+    {
+        
+        navMeshAgent.SetDestination(player.transform.position);
+        enemyAnimatorController.SetBool("isMoving", true);
+
+    }
+
+    void HeadIKSetup()
+    {
+        multiAimConstraint = mannequinEnemy.GetComponentInChildren<MultiAimConstraint>();
+        if (multiAimConstraint != null)
+        {
+            multiAimConstraint.weight = weight;
+        }
+        else
+        {
+            Debug.LogWarning("MultiAimConstraint not found on the specified mannequin: " + mannequinEnemy.name);
+        }
+    }
+
+    void HeadLockOnTarget()
+    {
+        multiAimConstraint = mannequinEnemy.GetComponentInChildren<MultiAimConstraint>();
+
+        if (multiAimConstraint != null)
+        {
+            
+            StartCoroutine(IncreaseWeightGradually(multiAimConstraint, weight + 1f, increaseDuration));
+            Debug.Log("Weight is increasing");
+
+        }
+        else
+        {
+            Debug.Log("MultiAim Constraint is null");
+        }
+    }
+
+    IEnumerator IncreaseWeightGradually(MultiAimConstraint constraint, float targetWeight, float duration)
+    {
+        float initialWeight = constraint.weight;
+        float timeElapsed = 0f;
+
+        while (timeElapsed < duration)
+        {
+
+            constraint.weight = Mathf.Lerp(initialWeight, targetWeight, timeElapsed / duration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        //Ensure the final weight is set exactly
+        constraint.weight = targetWeight;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw the ray in the scene view for visualization
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.DrawRay(eyes.transform.position, eyes.transform.forward * raycastDistance);
     }
 
 }
