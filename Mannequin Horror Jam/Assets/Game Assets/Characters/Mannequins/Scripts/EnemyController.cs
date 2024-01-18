be23f0866d58bdb4e3452c8eff3d77af07b8b18a
TheBossa <74020301+TheBossaaa@users.noreply.github.com>
2024-01-19 00:42:32 +0300

    Revert "Merge branch 'BossaWorkflow' of https://github.com/cartechr/Horror_Game_Jam into BossaWorkflow"
    This reverts commit 242195c845f0b57b04231bd255c4717cfec77029, reversing
    changes made to e9fa69307e3c72c8e50bcf8774f04f6aed7088d8.

--- Mannequin Horror Jam/Assets/Game Assets/Characters/Mannequins/Scripts/EnemyController.cs
+++ Mannequin Horror Jam/Assets/Game Assets/Characters/Mannequins/Scripts/EnemyController.cs
@@ -14,112 +14,31 @@ public class EnemyController : MonoBehaviour
     public Transform player;
     public LayerMask whatIsGround, whatIsPlayer;
 
-    [Header("Enemy Control Assignments")]
-    [Tooltip("Assign Enemy Object")]
-    [SerializeField] GameObject mannequinEnemy;
-    [Tooltip("Assign Player Here")]
-    [SerializeField] GameObject player;
-    [Tooltip("Eyes to emit raycast from")]
-    [SerializeField] GameObject eyes;
-    [Tooltip("Assign the NavMesh Agent (It's inside the mannequin)")]
-    [SerializeField] NavMeshAgent navMeshAgent;
-    [Tooltip("Assign the Enemy Animator Component")]
-    [SerializeField] Animator enemyAnimatorController;
-    [Tooltip("This will be pulled automatically")]
-    [SerializeField] MultiAimConstraint multiAimConstraint;
-    [Tooltip("Select the relevant layers")]
-    [SerializeField] LayerMask whatIsGround, whatIsPlayer;
+    [Header("Patrol Waypoints")]
+    [Tooltip("Assign Patrol Waypoints Here")]
+    public Transform[] patrolWaypoints;
+    public int currentWaypointIndex;
 
-    [Header("Enemy Control Variables")]
-    [Tooltip("Assign the speed for the enemy")]
-    [SerializeField] float moveSpeed;
-    [Tooltip("Delay between waypoints")]
-    [SerializeField] float delayBetween;
-    [Tooltip("Float variable for head turn DO NOT CHANGE")]
-    [SerializeField] float weight;
-    [Tooltip("Head turn weight increase duration")]
-    [SerializeField] float increaseDuration = 3f;
+    [Header("States and Distances for AI")]
+    public float greenAreaDistance;
+    public float yellowAreaDistance;
+    public float redAreaDistance;
+    public float attackDistance;
 
-    [Header("Adjust Ranges for the AI")]
-    [Tooltip("Distance that AI will only be ALERTED if player is sprinting")]
-    [SerializeField] float greenRange;
-    [Tooltip("Distance that AI only be ALERTED if player is walking and ATTACK if player is sprinting")]
-    [SerializeField] float yellowRange;
-    [Tooltip("Distance that AI will be ALERTED if the player is walking & crouching but ATTACK if the player is sprinting")]
-    [SerializeField] float redRange;
-    [Tooltip("Distance for AI attack range")]
-    [SerializeField] float attackRange; //This is to decide how far/close should the attack anim happen
+    public bool playerInGreenArea;
+    public bool playerInYellowArea;
+    public bool playerInRedArea;
+    public bool playerInAttackRange;
 
-    [Header("AI State Controls")]
-    [Tooltip("This AI will Patrol & Play Relevant Animations")]
-    [SerializeField] bool shouldPatrol;
-    [Tooltip("This AI will not patrol but Play Relevant Animations")]
-    [SerializeField] bool shouldIdle;
-    [Tooltip("Is this AI hostile?")]
-    [SerializeField] bool isHostile;
-
-    [Header("DEBUG CONTROLS")]
-    [Tooltip("This is for debugging and animation triggering")]
-    [SerializeField] bool isMoving;
-    [Tooltip("This is for debugging and animation triggering")]
-    [SerializeField] bool isChasingPlayer;
-    [Tooltip("To check if the player is in green range")]
-    [SerializeField] bool playerInGreenRange;
-    [Tooltip("To check if the player is in yellow range")]
-    [SerializeField] bool playerInYellowRange;
-    [Tooltip("To check if the player is in red range")]
-    [SerializeField] bool playerInRedRange;
-    [Tooltip("To check if the player is in attack range")]
-    [SerializeField] bool playerInAttackRange;
-    [Tooltip("Check if the player is sprinting")]
-    [SerializeField] bool playerIsSprinting;
+    private StarterAssetsInputs playerInputs;
 
     private void Awake()
     {
-        StarterAssetsInputs starterAssetsInputs = GetComponent<StarterAssetsInputs>();
-        playerIsSprinting = starterAssetsInputs.sprint;
-    }
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player").transform;
+        currentWaypointIndex = 0;
 
-
-    private void Update()
-    {
-        
-        //Checks for player if detected
-        playerInGreenRange = Physics.CheckSphere(transform.position, greenRange, whatIsPlayer);
-        playerInYellowRange = Physics.CheckSphere(transform.position, yellowRange, whatIsPlayer);
-        playerInRedRange = Physics.CheckSphere(transform.position, redRange, whatIsPlayer);
-
-        //Checks if the player is inside the attack range
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-
-        if (!playerInGreenRange && !playerIsSprinting) Patrolling();
-
-        if (playerInGreenRange || !playerInYellowRange
-            || !playerInRedRange) Patrolling();
-        //if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        //if (playerInAttackRange && playerInSightRange) AttackPlayer();
-
-
-    }
-
-    void Patrolling()
-    {
-
-    }
-
-    void ChasePlayer()
-    {
-
-    }
-
-    void AttackPlayer()
-    {
-
-    }
-
-    /*
-    void Patroling()
-    {
+        playerInputs = player.GetComponent<StarterAssetsInputs>();
 
     }
 
